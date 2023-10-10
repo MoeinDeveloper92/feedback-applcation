341cd504c3349fe1b3a4fd6fa115b3f190942f36
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-10-10 21:12:39 +0200

    adding feedbackstats

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
-const FeedbackItem = ({ item }) => {
+import { FaTimes } from "react-icons/fa"
+
+
+const FeedbackItem = ({ item, handleDelete }) => {
+
+    const handleClick = (id) => {
+        handleDelete(id)
+    }
 
     return (
         <Card >
             <div className="num-display">{item.rating}</div>
+            <button onClick={() => handleClick(item.id)} className='close'>
+                <FaTimes color='purple' />
+            </button>
             <div className="text-display">
                 {item.text}
             </div>
