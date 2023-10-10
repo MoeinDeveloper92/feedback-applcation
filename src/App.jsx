f046216b0bd3468d3489586b8ff5a1dc1902db86
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-10-10 21:12:47 +0200

    adding feedback stats

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,31 @@ import { useState } from 'react'
 import Header from "./components/Header"
 import { feedbackData } from "./data/feedbackData"
 import FeedbackList from './components/FeedbackList'
-
+import FeedbackStats from './components/FeedbackStats'
 function App() {
 
+
     const [feedback, setFeedback] = useState(feedbackData)
+
+    // it updates the state by remobing an item with the mentioned id.
+    const deleteFeedback = (id) => {
+        //we want to set the feedback to new array
+        if (window.confirm("Are you sure that you want to delete?")) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackList feedback={feedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
             </div>
         </>
     )
 }
 
 export default App
+
+
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import FeedbackItem from "./FeedbackItem"
 import PropTypes from 'prop-types'
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
     if (!feedback || feedback.length === 0) return <p>No Feedback Yet</p>
 
     return (
         <div className="feedbackList">
             {feedback.map((item) => (
-                <FeedbackItem item={item} key={item.id} />
+                <FeedbackItem item={item} key={item.id}
+                    handleDelete={handleDelete}
+                />
             ))}
         </div>
     )
