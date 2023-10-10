b9d5b27ff49e66e2942b30a429c8e5f95b73f57a
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-10-10 23:26:00 +0200

    adding form submission functionality

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import FeedbackItem from "./FeedbackItem"
-import PropTypes from 'prop-types'
+import React from 'react';
+import FeedbackItem from './FeedbackItem';
+import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+
 const FeedbackList = ({ feedback, handleDelete }) => {
-    if (!feedback || feedback.length === 0) return <p>No Feedback Yet</p>
+    if (!feedback || feedback.length === 0) return <p>No Feedback Yet</p>;
 
     return (
-        <div className="feedbackList">
-            {feedback.map((item) => (
-                <FeedbackItem item={item} key={item.id}
-                    handleDelete={handleDelete}
-                />
-            ))}
+        <div>
+            <div className="feedbackList">
+                {feedback.map((item) => (
+                    <motion.div
+                        key={item.id}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        // Exit specifies the state when the element is removed from the DOM
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        <FeedbackItem item={item} handleDelete={handleDelete} />
+                    </motion.div>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 FeedbackList.propTypes = {
-    feedback: PropTypes.array.isRequired
-}
+    feedback: PropTypes.array.isRequired,
+};
 
-export default FeedbackList
+export default FeedbackList;
