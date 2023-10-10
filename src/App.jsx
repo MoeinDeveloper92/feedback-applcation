77e22aa16cbfca417de022805f92c98d95eb4fe0
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-10-10 18:51:05 +0200

    adding header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,10 @@
-import React, { useState } from "react"
-
+import React from 'react'
 
 function App() {
-    const title = "Blog Post"
-    const body = "This is my blog post"
-
-    const comments = [
-        { id: 1, text: "Coments One" },
-        { id: 2, text: "Comment Two" },
-        { id: 3, text: "Comment Three" }
-    ]
-
-    const loading = false
-    const showCommetns = false
-
-
-
-    if (loading) return <h1>Loading...</h1>
-
     return (
-        <>
-            <div className="container">
-                <h1>{title.toUpperCase()}</h1>
-                <p>{body}</p>
-
-                {showCommetns && (<>
-                    <div className="comments">
-                        <h3>Commetns: {comments.length}</h3>
-                        <ul>
-                            {comments.map((cm, index) => {
-                                return <li key={index}>{cm.text}</li>
-                            })}
-                        </ul>
-                    </div>
-                </>)}
-
-            </div>
-        </>
-
+        <div className="container">
+            <h1>My App</h1>
+        </div>
     )
 }
 
